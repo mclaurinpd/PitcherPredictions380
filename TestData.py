9c78a38b5e357e00da25492948b37b33d78fbb8d
mclaurinpd <mclaurinpd@gmail.com>
2017-11-02 17:07:35 -0400

    TestData.py now reads in all the data

--- TestData.py
+++ TestData.py
@@ -1,22 +1,28 @@
 import csv
 import random
+import os
+from Pitcher import Pitcher
 '''
-Authors: Brenton Belanger, David Matrisciano II, and Phillip McLaurins
+Authors: Brenton Belanger, David Matrisciano II, and Phillip McLaurin
 '''
 
-def loadCSV(file,list):
-	with open(file, 'rb') as csvfile:
-	reader = csv.reader(f)
-	for row in spamreader:
-		tokens = row.split(',')
-                name = tokens[0]
-		whip = float(tokens[1])
-		era = float(tokens[2])
-		fip = float(tokens[3])
-		pitcher = Pitcher(name,whip,era,fip)
-		list.append(pitcher)
+def loadData(file,list):
+	with open(file, 'r') as file:
+		for line in file:
+			tokens = line.split(',')
+			name = tokens[0]
+			whip = float(tokens[1])
+			era = float(tokens[2])
+			fip = float(tokens[3])
+			pitcher = Pitcher(name,whip,era,fip)
+			list.append(pitcher)
 
 def main():
-	file = open("AllPitcherSet.csv","r")
-        allPitchers =[]
-	loadCSV(file,allPitchers)
+	file = os.getcwd() + "\\HOFSet.txt"
+	allPitchers =[]
+	loadData(file,allPitchers)
+	for x in allPitchers:
+		x.__str__()
+		print()
+
+main()
