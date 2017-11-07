e4f23b99f66bd3f9d38a66f6087452322961d229
dvm5070 <32747298+dvm5070@users.noreply.github.com>
2017-11-06 22:00:32 -0500

    random updated

--- randoms.py
+++ randoms.py
@@ -4,12 +4,16 @@ def randomword(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 def main():
-    for i in range(0,100000):
+    testPitchers = input("How many pitchers do you want? ")
+    fileName = input("Please enter a file name: (with .txt at end) ")
+    file = open(fileName,'w')
+    for i in range(0,testPitchers):
         names = randomword(10)
         whip = math.ceil(random.uniform(1.10,1.50)*100)/100
         fip = random.randint(50,150)
         era = random.randint(50,150)
         pitcher = str(str(names)+","+str(whip)+","+str(era)+","+str(fip))
         print(pitcher)
-
+        file.write(pitcher+"\n")
+    file.close()
 main()
